8f48a6aa9ce1fe36547ef2a8cd5b8dec4c01700d
tanvir-hasan <tanvir8661@gmail.com>
2024-03-17 23:11:18 +0600

    normal variable all

--- Day 1/basicSyntax.py
+++ Day 1/basicSyntax.py
@@ -20,3 +20,32 @@ a, b, c = 5, 3.2, 'Hello'
 print(a)  # prints 5
 print(b)  # prints 3.2
 print(c)  # prints Hello 
+site1 = site2  = 'programiz.com'
+
+print(site1)  # prints programiz.com
+print(site2)  # prints programiz.com
+
+# list literal
+fruits = ["apple", "mango", "orange"] 
+print(fruits)
+
+# tuple literal
+numbers = (1, 2, 3) 
+print(numbers)
+
+# dictionary literal
+alphabets = {'a':'apple', 'b':'ball', 'c':'cat'} 
+print(alphabets)
+
+# set literal
+vowels = {'a', 'e', 'i' , 'o', 'u'} 
+print(vowels)
+
+num1 = 5
+print(num1, 'is of type', type(num1))
+
+num2 = 2.0
+print(num2, 'is of type', type(num2))
+
+num3 = 1+2j
+print(num3, 'is of type', type(num3))
