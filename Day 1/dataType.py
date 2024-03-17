d391378a953c551620d1ff8169b6d2dc469ac77d
tanvir-hasan <tanvir8661@gmail.com>
2024-03-17 23:13:44 +0600

    Basic Data Types of SET

--- Day 1/dataType.py
+++ Day 1/dataType.py
@@ -14,3 +14,12 @@ print(product[0])   # Microsoft
 
 # access element at index 1
 print(product[1])   # Xbox
+
+# create a set named student_id
+student_id = {112, 114, 116, 118, 115}
+
+# display student_id elements
+print(student_id)
+
+# display type of student_id
+print(type(student_id))
