a4ea6c1183189e9233357a52d181683294070355
tanvir-hasan <tanvir8661@gmail.com>
2024-03-17 23:12:54 +0600

    Basic Data Types

---
+++ Day 1/dataType.py
@@ -0,0 +1,16 @@
+languages = ["Swift", "Java", "Python"]
+
+# access element at index 0
+print(languages[0])   # Swift
+
+# access element at index 2
+print(languages[2])   # Python
+
+# create a tuple 
+product = ('Microsoft', 'Xbox', 499.99)
+
+# access element at index 0
+print(product[0])   # Microsoft
+
+# access element at index 1
+print(product[1])   # Xbox
